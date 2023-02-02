10903c9facf0731381f91f7cd3dde2f00e900aac
Aditi <aditijaiswal275@gmail.com>
2023-02-02 15:16:04 +0530

    Map method

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,22 +3,24 @@ function Home(){
     
     const [restro,setRestro]  = useState([]);
     
-    async function getData() {
-        const response = await fetch("https://my-json-server.typicode.com/aditijais/mockjson/restaurants");
-        const data = await response.json();
-        setRestro(data);
-        console.log(data);
-    }
+    
         useEffect(() => {
         	getData();
-		    
-	     }, [restro]);
+		    async function getData() {
+                const response = await fetch("https://my-json-server.typicode.com/aditijais/mockjson/restaurants");
+                const data = await response.json();
+                setRestro(data);
+                console.log(data);
+            }
+	     }, []);
     
     return(
       <div>{
         
                  restro.map((data)=>{
-                    <div>{data[0].name}</div>
+                    return(
+                    <div>{data.name}</div>
+                    )
                  })
                
             
